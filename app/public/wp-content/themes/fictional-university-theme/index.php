6aede575a1c2609ef0bc83c3231c613135cc48c0
Allison Neuman <allineu@allisons-mbp.attlocal.net>
2019-03-11 16:19:01 -0500

    add a loop to the index php file

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,11 @@
-This is our amazing custom theme
+<?php
+  $names = array('Brad', 'John', 'Jane');
+
+  $count = 1;
+
+  while($county < count($names)) {
+    echo "<li>Hi, my name is $names[$count] </li>";
+    $count++;
+
+  }
+?>
