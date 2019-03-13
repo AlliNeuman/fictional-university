97bbdf69608232ef51d23993b012d318f0ee318e
Allison Neuman <alli.neuman@gmail.com>
2019-03-13 10:05:25 -0500

    add styling to inner pages and transfer static content to php for dynamic info

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -9,4 +9,11 @@ function university_files() {
 
 add_action('wp_enqueue_scripts', 'university_files');
 
+// This adds the title tag to the browser tab
+function university_features() {
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'university_features');
+
 ?>
