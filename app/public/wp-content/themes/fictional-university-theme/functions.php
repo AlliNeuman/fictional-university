08e5398472d4bcdb97ff8a030c6c1681e07ea8b3
Allison Neuman <alli.neuman@gmail.com>
2019-03-14 15:45:47 -0500

    remove dynamic menu and add custom menu that has a current page view for about us

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -11,9 +11,9 @@ add_action('wp_enqueue_scripts', 'university_files');
 
 // This adds the title tag to the browser tab
 function university_features() {
-  register_nav_menu('headerMenuLocation', 'Header Menu Location');
-  register_nav_menu('footerLocationOne', 'Footer Location One');
-  register_nav_menu('footerLocationTwo', 'Footer Location Two');
+  // register_nav_menu('headerMenuLocation', 'Header Menu Location');
+  // register_nav_menu('footerLocationOne', 'Footer Location One');
+  // register_nav_menu('footerLocationTwo', 'Footer Location Two');
   add_theme_support('title-tag');
 }
 
