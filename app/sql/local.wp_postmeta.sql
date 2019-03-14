3fea34d616e344d1e0b8eefa2946ec83beb5c245
Allison Neuman <alli.neuman@gmail.com>
2019-03-14 15:33:23 -0500

    add dynamic menus

---
+++ app/sql/local.wp_postmeta.sql
@@ -0,0 +1,25 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_postmeta` VALUES
+(1,2,"_wp_page_template","default"),
+(2,3,"_wp_page_template","default"),
+(3,5,"_edit_last","1"),
+(4,5,"_edit_lock","1552339732:1"),
+(9,7,"_edit_last","1"),
+(10,7,"_edit_lock","1552340289:1"),
+(13,9,"_edit_last","1"),
+(14,9,"_edit_lock","1552488295:1"),
+(15,11,"_edit_last","1"),
+(16,11,"_edit_lock","1552495769:1"),
+(17,13,"_edit_last","1"),
+(18,13,"_edit_lock","1552489564:1"),
+(19,15,"_edit_last","1"),
+(20,15,"_edit_lock","1552489822:1"),
+(21,17,"_edit_last","1"),
+(22,17,"_edit_lock","1552495796:1"),
+(23,3,"_wp_trash_meta_status","draft"),
+(24,3,"_wp_trash_meta_time","1552495921"),
+(25,3,"_wp_desired_post_slug","privacy-policy"),
+(26,20,"_edit_last","1"),
+(27,20,"_edit_lock","1552496070:1");
---
+++ app/sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"Alli","$P$Brn/390KQI6QgsvvTqbTGZDTzW/Eel1","alli","alli.neuman@gmail.com","","2019-03-11 19:13:03","",0,"Alli");
