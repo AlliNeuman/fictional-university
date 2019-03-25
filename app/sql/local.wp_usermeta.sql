75ab4b1abed4433f5536288338d5754b628cf048
Allison Neuman <alli.neuman@gmail.com>
2019-03-25 12:29:22 -0500

    add custom post type as events to mu-plugins for must used plugins required by the theme

--- app/sql/local.wp_usermeta.sql
+++ app/sql/local.wp_usermeta.sql
@@ -5,7 +5,7 @@ INSERT INTO `wp_usermeta` VALUES
 (1,1,"nickname","Alli"),
 (2,1,"first_name",""),
 (3,1,"last_name",""),
-(4,1,"description",""),
+(4,1,"description","I am a full stack web developer"),
 (5,1,"rich_editing","true"),
 (6,1,"syntax_highlighting","true"),
 (7,1,"comment_shortcuts","false"),
@@ -17,6 +17,9 @@ INSERT INTO `wp_usermeta` VALUES
 (13,1,"wp_user_level","10"),
 (14,1,"dismissed_wp_pointers","wp496_privacy"),
 (15,1,"show_welcome_panel","1"),
-(16,1,"session_tokens","a:1:{s:64:\"312b232aa3774a7216c00eb0a47224a673b3c68bab2bfe357667fa47285e5520\";a:4:{s:10:\"expiration\";i:1552504657;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:121:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36\";s:5:\"login\";i:1552331857;}}"),
-(17,1,"wp_dashboard_quick_press_last_post_id","4"),
-(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}");
+(16,1,"session_tokens","a:1:{s:64:\"b7c6d083177d3dd984a8644cb8ba43e3e6458e11e074a9e95c7ada3081482fc2\";a:4:{s:10:\"expiration\";i:1553109420;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:121:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36\";s:5:\"login\";i:1552936620;}}"),
+(17,1,"wp_dashboard_quick_press_last_post_id","38"),
+(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
+(19,1,"managenav-menuscolumnshidden","a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}"),
+(20,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}"),
+(21,1,"nav_menu_recently_edited","4");
