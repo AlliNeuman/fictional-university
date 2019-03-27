996be2a4b1b91db81a1427b688e13f3031cde83f
Allison Neuman <alli.neuman@gmail.com>
2019-03-27 11:51:44 -0500

    add dynamic events to front-page.php, styling for archive-event.php and update params for mu-plugins file for registering the new events objects

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,8 @@
 <?php
 function university_post_types() {
   register_post_type('event', array(
+    'rewrite' => array('slug' => 'events'),
+    'has_archive' => true,
     'public' => true,
     'labels' => array(
       'name' => 'Events',
@@ -12,6 +14,7 @@ function university_post_types() {
     'menu_icon' => 'dashicons-calendar'
   ));
 }
+
 add_action('init', 'university_post_types');
 
 ?>
