a1754fedb9768803648ce77125b315f27c47d0bf
Allison Neuman <alli.neuman@gmail.com>
2019-03-27 15:29:49 -0500

    add custom fields for event date and php to front-page.php'

--- app/sql/local.wp_usermeta.sql
+++ app/sql/local.wp_usermeta.sql
@@ -17,9 +17,11 @@ INSERT INTO `wp_usermeta` VALUES
 (13,1,"wp_user_level","10"),
 (14,1,"dismissed_wp_pointers","wp496_privacy"),
 (15,1,"show_welcome_panel","1"),
-(16,1,"session_tokens","a:1:{s:64:\"5e29fdd40a3924fb07bc1746cba745fcc773fcb7a098fd2d8c77b8aa61f6ff06\";a:4:{s:10:\"expiration\";i:1553706649;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:121:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36\";s:5:\"login\";i:1553533849;}}"),
+(16,1,"session_tokens","a:1:{s:64:\"5495a8b6a703ad606a46b18a677a6dd5cb583bbbeed104db0701936d77488c1d\";a:4:{s:10:\"expiration\";i:1553887942;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:121:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36\";s:5:\"login\";i:1553715142;}}"),
 (17,1,"wp_dashboard_quick_press_last_post_id","47"),
 (18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
 (19,1,"managenav-menuscolumnshidden","a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}"),
 (20,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}"),
-(21,1,"nav_menu_recently_edited","4");
+(21,1,"nav_menu_recently_edited","4"),
+(22,1,"closedpostboxes_post","a:0:{}"),
+(23,1,"metaboxhidden_post","a:7:{i:0;s:12:\"revisionsdiv\";i:1;s:13:\"trackbacksdiv\";i:2;s:10:\"postcustom\";i:3;s:16:\"commentstatusdiv\";i:4;s:11:\"commentsdiv\";i:5;s:7:\"slugdiv\";i:6;s:9:\"authordiv\";}");
